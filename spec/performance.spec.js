f77fcc7071d34974a82a1d9c733c7c8d6ae8dc58
Jens Keller <jens.keller@sap.com>
2023-06-25 07:33:45 +0200

    performance tests for character retrieval based on submissions

--- spec/performance.spec.js
+++ spec/performance.spec.js
@@ -1,8 +1,11 @@
 import fs from 'fs/promises'
 import fssync from 'fs'
+import FsRepository from '../fs-repository.js'
+import Controller from '../controller.js'
+import WinstonLogger from '../winston-logger.js'
 
 describe('Performance', () => {
-    it('Finishes each operation in less than 0.1 seconds', async () => {
+    it('has a response time of less than 0.1 seconds for read/write operations', async () => {
         let starTime = Date.now()
         const characters = JSON.parse((await fs.readFile('init/characters.json')).toString())
         expect(Date.now() - starTime).toBeLessThan(100)
@@ -11,11 +14,11 @@ describe('Performance', () => {
         starTime = Date.now()
         await fs.writeFile('spec/characters.json.tmp', JSON.stringify(characters, null, '\t'))
         expect(Date.now() - starTime).toBeLessThan(100)
-    
+
         starTime = Date.now()
         fssync.writeFileSync('spec/characters.csv.tmp', characters.map(c => Object.values(c).join('\t')).join('\n'))
         expect(Date.now() - starTime).toBeLessThan(100)
-    
+
         starTime = Date.now()
         const cContent = (fssync.readFileSync('spec/characters.csv.tmp')).toString()
         const charsFromCsv = cContent.split('\n').map(line => {
@@ -24,5 +27,20 @@ describe('Performance', () => {
         })
         expect(Date.now() - starTime).toBeLessThan(100)
         expect(charsFromCsv.length).toBe(8105)
+    })
+
+    it('has a response time of less than 1 second for large submission pools', async () => {
+        global.logger = { info: () => null, warn: () => null, error: () => null }
+        const repo = new FsRepository('spec/tmp')
+        repo.save = () => null
+        const controller = new Controller(repo)
+
+        for (let i = 0; i < 20000; i++) {
+            const character = repo.characters[Math.floor(repo.characters.length * Math.random())].character
+            const remembered = Boolean(Math.floor(2 * Math.random()))
+            controller.submit({ character, remembered, meaning: '', words: '', related: '' })
+        }
+
+        controller.getNext()
     })
 })
