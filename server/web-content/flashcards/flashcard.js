125915ec148b9b9faba3538560e35a080c931ed8
Jens Keller <keller_jens@gmx.de>
2025-06-08 16:16:45 +0800

    few more simplfications and fixes

--- server/web-content/flashcards/flashcard.js
+++ server/web-content/flashcards/flashcard.js
@@ -2,36 +2,35 @@ import { html, useState, useEffect } from '/preact-htm-standalone.js'
 import CharacterInfo from './character-info.js'
 import ExpressionInfo from './expression-info.js'
 
-const makeHash = (deck, hanzi, flipped = false) => `#flashcards/${deck}/${hanzi}${flipped ? '/flipped' : ''}`
+const makeHash = (deck, hanzi, flipped = '') => `#flashcards/${deck}/${hanzi}${flipped}`
 
 const parseHash = () => {
     const match = window.location.hash.match(/^#flashcards\/([^/]+)(?:\/([^/]+))?(?:\/(flipped))?$/)
     return match
-        ? { deck: decodeURIComponent(match[1]), hanzi: match[2] ? decodeURIComponent(match[2]) : '', flipped: !!match[3] }
-        : { deck: '', hanzi: '', flipped: false }
+        ? { deck: decodeURIComponent(match[1]), hanzi: match[2] ? decodeURIComponent(match[2]) : '', flipped: match[3] || '' }
+        : { deck: '', hanzi: '', flipped: '' }
 }
 
 const fetchNextHanziFromDeck = (deck) => fetch(`/api/flashcards/${deck}`).then(res => res.json())
 const itemUrlPath = (hanzi) => (hanzi.length === 1) ? 'characters' : 'expressions'
 const fetchItemDetails = (hanzi) => fetch(`/api/${itemUrlPath(hanzi)}/${hanzi}`).then(res => res.json())
 
-// TODO make flipped a string
 export default function Flashcard({ user }) {
-    const [isFlipped, setFlipped] = useState(!!parseHash().flipped)
-    const [selectedDeck, setSelectedDeck] = useState('')
-    const [currentItem, setCurrentItem] = useState(null)
     const [decks, setDecks] = useState([])
+    const [selectedDeck, setSelectedDeck] = useState('')
     const [hanzi, setHanzi] = useState('')
+    const [currentItem, setCurrentItem] = useState(null)
+    const [flipped, setFlipped] = useState(parseHash().flipped)
 
-    const setStateFromHash = async () => {
+    const setStateFromHash = async (availableDecks) => {
         const hash = parseHash()
         const deck = hash.deck || localStorage.getItem('selectedDeck') || (availableDecks || [])[0]?.name || ''
         const hanzi = hash.hanzi || (deck ? await fetchNextHanziFromDeck(deck) : '')
         localStorage.setItem('selectedDeck', deck)
         setSelectedDeck(deck)
         setHanzi(hanzi)
-        setFlipped(!!hash.flipped)
-        !!hash.flipped ? setCurrentItem(await fetchItemDetails(hanzi)) : setCurrentItem(null)
+        setFlipped(hash.flipped)
+        hash.flipped ? setCurrentItem(await fetchItemDetails(hanzi)) : setCurrentItem(null)
         history.replaceState(null, '', `#flashcards/${deck}/${hanzi}${hash.flipped ? '/flipped' : ''}`)
     }
 
@@ -39,24 +38,18 @@ export default function Flashcard({ user }) {
         const resp = await fetch('/api/decks')
         const availableDecks = await resp.json()
         setDecks(availableDecks)
-        setStateFromHash()
-    }, [])
+        setStateFromHash(availableDecks)
 
-    useEffect(() => {
         const onHashChange = () => window.location.hash.startsWith('#flashcards') ? setStateFromHash() : null
         window.addEventListener('hashchange', onHashChange)
         return () => window.removeEventListener('hashchange', onHashChange)
     }, [])
 
-    const switchDeck = async (deck) => {
-        localStorage.setItem('selectedDeck', deck)
-        const hanzi = await fetchNextHanziFromDeck(deck)
-        window.location.hash = makeHash(deck, hanzi, false)
-    }
-    
+    const switchDeck = (deck) => fetchNextHanziFromDeck(deck).then(hanzi => window.location.hash = makeHash(deck, hanzi))
+
     const saveRelated = async (newRelated) => {
         if (!user.username) return alert('You must be logged in to make changes.')
-            
+
         const headers = { 'Content-Type': 'application/json' }
         const body = JSON.stringify({ ...currentItem, related: newRelated })
         await fetch(`/api/characters/${currentItem.hanzi}`, { method: 'PUT', headers, body })
@@ -69,12 +62,10 @@ export default function Flashcard({ user }) {
         const headers = { 'Content-Type': 'application/json' }
         const body = JSON.stringify({ hanzi, result })
         const nextHanzi = await (await fetch(`/api/flashcards/${selectedDeck}`, { method: 'POST', headers, body })).json()
-        window.location.hash = makeHash(selectedDeck, nextHanzi, false)
+        window.location.hash = makeHash(selectedDeck, nextHanzi)
     }
 
-    const flip = async () => {
-        window.location.hash = makeHash(selectedDeck, hanzi, !isFlipped)
-    }
+    const flipCard = () => window.location.hash = makeHash(selectedDeck, hanzi, flipped ? '' : '/flipped')
 
     return html`
         <style>
@@ -116,7 +107,7 @@ export default function Flashcard({ user }) {
                 ${decks.map(deck => html`<option value=${deck.name}>${deck.name}</option>`)}
             </select>
         </div>
-        <div class='card ${isFlipped ? 'flipped' : ''}' onclick=${flip} style='display: flex; flex-direction: column; width: 100%; aspect-ratio: 2 / 3; border-radius: .4em; box-shadow: 0 25px 50px -12px rgb(0 0 0 / .25); background: linear-gradient(to bottom right, #7EA5C5 30%, #486A8D 100%);'>
+        <div class='card ${flipped}' onclick=${flipCard} style='display: flex; flex-direction: column; width: 100%; aspect-ratio: 2 / 3; border-radius: .4em; box-shadow: 0 25px 50px -12px rgb(0 0 0 / .25); background: linear-gradient(to bottom right, #7EA5C5 30%, #486A8D 100%);'>
             <div class='card-front' style='display: flex; align-items: center; justify-content: center; font-size: ${8 / hanzi.length}em;'>
                 <div style='user-select: none; color: white;'><strong>${hanzi}</strong></div>
             </div>
